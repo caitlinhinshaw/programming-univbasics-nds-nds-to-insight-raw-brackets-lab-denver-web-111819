939963d47d1699c1f14702cddee9a564ddcf49cc
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 05:18:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def directors_totals(nds)
   movie_index = 0
   sum = 0
   while director_index < nds.length
-    key = nds[:name].join
+    key = "#{nds[:name]}"
     while movie_index < nds[director_index][:movies].length
       sum += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
